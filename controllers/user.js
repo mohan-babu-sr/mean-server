d9a05f7219ed2474a81fcfaa8772b885f3faa3c5
MOHAN BABU S R <writeto.mohanbabu@gmail.com>
2023-05-23 18:57:41 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -39,7 +39,7 @@ exports.userLogin = (req, res, next) => {
                 message: 'Auth failed!'
             })
         }
-        const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id }, process.env.JWT_KEY, { expiresIn: '1h' });
+        const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id }, 'secret_this_should_be_longer', { expiresIn: '1h' });
         res.status(200).json({
             token: token,
             expiresIn: 3600,
